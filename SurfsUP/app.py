1e35f77e42e18e633f014c77b4fac95faaedc2bc
Darren Holzhausen <darrenholzhausen@gmail.com>
2025-01-23 15:59:26 -0600

    update

--- SurfsUP/app.py
+++ SurfsUP/app.py
@@ -3,7 +3,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from flask import Flask, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 import datetime as dt
 
 #################################################
