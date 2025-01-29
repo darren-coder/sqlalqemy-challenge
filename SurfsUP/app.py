b94cea86d831510e25f2891c38964fb0e45c2d58
Darren Holzhausen <darrenholzhausen@gmail.com>
2025-01-28 19:29:51 -0600

    update

--- SurfsUP/app.py
+++ SurfsUP/app.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from flask import Flask, jsonify
 from sqlalchemy import create_engine, func
-from datetime import datetime
+from datetime import datetime, strptime
 
 #################################################
 # Database Setup
@@ -21,9 +21,6 @@ Base.classes.keys()
 # Save references to each table
 station = Base.classes.station
 measurement = Base.classes.measurement
-
-# Create our session (link) from Python to the DB
-#session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -45,10 +42,10 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"Enter a start date after url below to retrieve tobs information "
-        f"using format: '/YYYY-MM-DD'<br/>"
+        f"using this format: /YYYY-MM-DD<br/>"
         f"/api/v1.0/start_date/<start_date><br/>"
         f"Enter start date/end date after url below for tobs information "
-        f"using this format: '/YYYY-MM-DD/YYYY-MM-DD'<br/>"
+        f"using this format: /YYYY-MM-DD/YYYY-MM-DD<br/>"
         f"/api/v1.0/start_date/end_date/<start_date>/<end_date>"
     )
 
@@ -58,15 +55,20 @@ def welcome():
 def precipitation():
     """Previous year of temperature observations."""
     
+    # Select Dates    
+    previous_year = datetime.datetime(2017,8,23) - datetime.timedelta(days=365)
+    
+    # Open session / make query / close session
+
     session = Session(engine)
 
-    previous_year = datetime.date(2017,8,23) - datetime.timedelta(days=365)
-    
     precip = session.query(measurement.date, measurement.prcp).\
         filter(measurement.date >= previous_year).\
         order_by(measurement.date).all()
 
     session.close()
+
+    # 
 
     all_measures = []
     for d, p in precip:
@@ -112,6 +114,8 @@ def tobs():
         filter(measurement.date >= previous_year).\
         filter(measurement.station == most_active_station).all()
 
+    session.close()
+
     most_active_temp_list = []
     for d, t in most_active_data:
         most_active_temp_data = {}
@@ -131,27 +135,25 @@ def start_from(start_date):
 
     session = Session(engine)
 
-    tobs_from = session.query(measurement.date, 
-                              func.min(measurement.tobs),\
-                              func.avg(measurement.tobs),\
-                                func.max(measurement.tobs)).\
-                                filter(measurement.date >= start_object).\
-                                group_by(measurement.date)
-
+    sel =  [func.min((measurement.tobs).label('Minimum Temp')),
+           func.avg((measurement.tobs).label('Average Temp')),
+           func.max((measurement.tobs).label('Maximum Temp'))]
+    
+    start = session.query(*sel).\
+        filter(measurement.date >= start_object)
+    
     session.close()
 
-    print(tobs_from)
+    print(start)
 
     tobs_from_list = []
-    for d, min, avg, max in tobs_from:
+    for min, avg, max in start:
         tobs_from_data = {}
-        tobs_from_data["Date"] = d
         tobs_from_data["Minimum Temperature"] = min
         tobs_from_data["Average Temperature"] = round(avg, 1)
         tobs_from_data["Maximum Temperature"] = max
         tobs_from_list.append(tobs_from_data)
 
-    print(tobs_from_list)
     return jsonify(tobs_from_list)
 
 # Start and End Date temperature observations.
@@ -160,7 +162,6 @@ def start_from(start_date):
 def start_end(start_date, end_date):
     """Temperature observations within given date range"""
 
-
     start_object = datetime.strptime(start_date, "%Y-%m-%d").date()
     end_object = datetime.strptime(end_date, "%Y-%m-%d").date()
     print(start_object)
@@ -168,33 +169,25 @@ def start_end(start_date, end_date):
     
     session = Session(engine)
 
-    tobs_from_to = session.query( 
-    measurement.date,
-    func.min(measurement.tobs).label('min_1'),
-    func.avg(measurement.tobs).label('avg_1'),
-    func.max(measurement.tobs).label('max_1')).\
-    filter(measurement.date >= start_object,
-            measurement.date <= end_object).all() 
-
-    print(tobs_from_to)
-
-    session.close()
-
+    sel =  [func.min((measurement.tobs).label('Minimum Temp')),
+           func.avg((measurement.tobs).label('Average Temp')),
+           func.max((measurement.tobs).label('Maximum Temp'))]
     
-
+    start_to_end = session.query(*sel).\
+        filter(measurement.date >= start_object,
+               measurement.date <= end_object).all()
+    
+    session.close()
+    
     tobs_from_to_list = []
-    for d, min, avg, max in tobs_from:
+    for  min, avg, max in start_to_end:
         tobs_from_to_data = {}
-        tobs_from_to_data["Date"] = d
         tobs_from_to_data["Minimum Temperature"] = min
         tobs_from_to_data["Average Temperature"] = round(avg, 1)
         tobs_from_to_data["Maximum Temperature"] = max
         tobs_from_to_list.append(tobs_from_to_data)
 
-    
-
     return jsonify(tobs_from_to_list)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
